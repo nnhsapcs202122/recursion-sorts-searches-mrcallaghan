38eca38ae11e8ac0c9c523e730d5a0a1267d3f4e
Matt Callaghan <mcallaghan@naperville203.org>
2022-03-10 10:58:58 -0600

    updated

--- StringReverse.java
+++ StringReverse.java
@@ -7,25 +7,38 @@
  */
 public class StringReverse
 {
-    
+
     public static String reverse(String str)
     {
-    
+
         // must have a terminating case
         if(str.equals(""))
         {
             return str;
         }
-        
+
         String firstChar = str.substring(0,1);
         String restOfString = str.substring(1);
-        
+
         // recurse - call this method with the simpler problem
         String restOfStringReversed = reverse(restOfString);
-        
+
         String stringReversed = restOfStringReversed + firstChar;
-        
+
         return stringReversed;
-    
+
+    }
+
+    public static String reverseIter(String str)
+    {
+        String strReversed = "";
+
+        for(int i = 0; i < str.length(); i++)
+        {
+            strReversed = str.substring(i, i + 1) + strReversed;
+        }
+
+        return strReversed;
+
     }
 }
